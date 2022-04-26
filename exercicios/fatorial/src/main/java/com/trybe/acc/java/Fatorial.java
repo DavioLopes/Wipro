a460496819f0e1c74925b9c02ae869385a1dcc37
Davio <davioplp@gmail.com>
2022-04-26 09:59:40 -0300

    feito

---
+++ exercicios/fatorial/src/main/java/com/trybe/acc/java/Fatorial.java
@@ -0,0 +1,13 @@
+package com.trybe.acc.java;
+
+public class Fatorial {
+
+  public static void main(String[] args) {
+    //Seu código aqui
+		Scanner scan = new Scanner(System.in);
+		
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println("O fatorial de 18 é igual a " + fatorial18);
+
+  }
+}
